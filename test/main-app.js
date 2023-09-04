dc888119ab610ed18961bda73736606a485d48a7
Varad Dhat <dvarad2607@gmail.com>
2023-09-04 16:52:14 -0700

    Implemented all methods to reduce sensitivity

--- test/main-app.js
+++ test/main-app.js
@@ -7,84 +7,6 @@ import DataManager from '../dist/dataManager.js';
 SpaceDriver.configureCallback(processInput)
 
 
-
-
-
-// // Scene
-// const scene = new THREE.Scene();
-// function setCameraToInitialState() {
-//     camera.position.set(30, 5, 2);
-//     camera.lookAt(0, 0, 0);
-// }
-// function frametoinistialstate(){
-//     mergedMesh.position.x=0;
-//     mergedMesh.position.y=0;
-//     mergedMesh.position.z=0;
-//     mergedMesh.rotation.x =0;
-//     mergedMesh.rotation.y =0;
-//     mergedMesh.rotation.z =0;
-
-// }
-
-
-// //Red X-axis
-// const curveX = new THREE.LineCurve3(
-//     new THREE.Vector3(0, 0, 0),
-//     new THREE.Vector3(10, 0, 0)
-// );
-// const geometryX = new THREE.TubeBufferGeometry(curveX, 64, 0.1, 8, false);
-// const materialX = new THREE.MeshLambertMaterial({color: 0xff0000});
-// const meshX = new THREE.Mesh(geometryX, materialX);
-
-
-// // Green Y-axis
-// const curveY = new THREE.LineCurve3(
-//     new THREE.Vector3(0, 0, 0),
-//     new THREE.Vector3(0, 10, 0)
-// );
-// const geometryY = new THREE.TubeBufferGeometry(curveY, 64, 0.1, 8, false);
-// const materialY = new THREE.MeshLambertMaterial({color: 0x00ff00});
-// const meshY = new THREE.Mesh(geometryY, materialY);
-
-
-// // // Blue Z-axis
-// const curveZ = new THREE.LineCurve3(
-//     new THREE.Vector3(0, 0, 0),
-//     new THREE.Vector3(0, 0, -10)
-// );
-// const geometryZ = new THREE.TubeBufferGeometry(curveZ, 64, 0.1, 8, false);
-// const materialZ = new THREE.MeshLambertMaterial({color: 0x0000ff});
-// const meshZ = new THREE.Mesh(geometryZ, materialZ);
-
-
-
-
-// function setVertexColors(geometry, color) {
-//     const colors = [];
-//     for (let i = 0; i < geometry.attributes.position.count; i++) {
-//         colors.push(color.r, color.g, color.b);
-//     }
-//     geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
-// }
-
-// // Set vertex colors for each geometry
-// setVertexColors(geometryX, new THREE.Color(0xff0000));
-// setVertexColors(geometryY, new THREE.Color(0x0000ff));
-// setVertexColors(geometryZ, new THREE.Color(0x00ff00));
-
-// // Merge the geometries
-// const mergedGeometry = BufferGeometryUtils.mergeBufferGeometries([geometryX, geometryY, geometryZ]);
-
-// // Use a material that respects vertex colors
-// const mergedMaterial = new THREE.MeshBasicMaterial({ vertexColors: true });
-
-// const mergedMesh = new THREE.Mesh(mergedGeometry, mergedMaterial);
-// scene.add(mergedMesh);
-
-// Camera
-const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.6, 2000);
-let globalCSVData = [];
-
 function softmax(Tx, Ty, Tz) {
     let sum = (Tx) + (Ty) + (Tz);
 
@@ -109,29 +31,65 @@ function softmax_r(Rx, Ry, Rz) {
     };
 }
 
-
-
+function computePosition(value, threshold, growthRateAbove, baseBelow) {
+    if (value < threshold) {
+        return Math.pow(baseBelow, value);
+    } else {
+        return Math.pow(baseBelow, threshold) + growthRateAbove * (value - threshold);
+    }
+}
 function processInput(report) {
 
+
+
     if (report.Tx === 0 && report.Ty === 0 && report.Tz === 0) {
+        Scene.mergedMesh.position.x = 0
+        Scene.mergedMesh.position.z = 0
+        Scene.mergedMesh.position.y = 0
+        console.log("TXX "+Scene.mergedMesh.position.x + " TYY " +Scene.mergedMesh.position.y +" TZZ " +Scene.mergedMesh.position.z ); // To log all positions at once
+        DataManager.pushData(['TXX', Scene.mergedMesh.position.x, 'TYY', Scene.mergedMesh.position.y, 'TZZ', Scene.mergedMesh.position.z]);
         Scene.frametoinistialstate();
     } else {
+
+        //Observation 3
+        if(report.Tx <= 50 || report.Ty <=50 || report.Tz <= 50){
+            Scene.frametoinistialstate();
+
+
+
+        }
+
+        //Using Softmax (Observation 1)
         let { wx, wy, wz } = softmax(report.Tx, report.Ty, report.Tz);
         //console.log(`XX: ${wx}, YY: ${wy}, ZZ: ${wz}`);
         
-        // //Using Softmax
+        // 
         // Scene.mergedMesh.position.x = report.Tx * wx / 100;
         // Scene.mergedMesh.position.z = report.Ty * wy / 100;
         // Scene.mergedMesh.position.y = report.Tz * wz / 100;
 
         //Observation 2
+        // const threshold = 175;
+        // const growthRateAboveThreshold = 10; // Linear growth rate for values above the threshold
+        // const baseBelowThreshold = 1.02; // Exponential growth rate for values below the threshold
+        
+        // Scene.mergedMesh.position.x = computePosition(report.Tx, threshold, growthRateAboveThreshold, baseBelowThreshold);
+        // Scene.mergedMesh.position.y = computePosition(report.Ty, threshold, growthRateAboveThreshold, baseBelowThreshold);
+        // Scene.mergedMesh.position.z = computePosition(report.Tz, threshold, growthRateAboveThreshold, baseBelowThreshold);
+        if(report.Tx >= 175 || report.Ty >= 175 || report.Tz >= 175){
+            Scene.mergedMesh.position.x = report.Tx/1.5
+            Scene.mergedMesh.position.z = report.Ty/1.5
+            Scene.mergedMesh.position.y = report.Tz/1.5
+            console.log("varad")
+        }
+        else{
+            Scene.mergedMesh.position.x = report.Tx/2
+            Scene.mergedMesh.position.z = report.Ty/2
+            Scene.mergedMesh.position.y = report.Tz/2
+        }
 
-        Scene.mergedMesh.position.x = report.Tx/10
-        Scene.mergedMesh.position.z = report.Ty/10
-        Scene.mergedMesh.position.y = report.Tz/10
-
+        
         console.log("TXX "+Scene.mergedMesh.position.x + " TYY " +Scene.mergedMesh.position.y +" TZZ " +Scene.mergedMesh.position.z ); // To log all positions at once
-        //globalCSVData.push(['TXX', Scene.mergedMesh.position.x, 'TYY', Scene.mergedMesh.position.y, 'TZZ', Scene.mergedMesh.position.z]);
         DataManager.pushData(['TXX', Scene.mergedMesh.position.x, 'TYY', Scene.mergedMesh.position.y, 'TZZ', Scene.mergedMesh.position.z]);
     }
 
@@ -184,33 +142,3 @@ function downloadAccumulatedData() {
 }
 
 
-
-// const axesHelper = new THREE.AxesHelper(10);  // 5 is the size; adjust as needed
-// scene.add(axesHelper);
-
-
-// Renderer
-// const renderer = new THREE.WebGLRenderer({antialias: true});
-// renderer.setClearColor("#233143"); // Set background colour
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// document.body.appendChild(renderer.domElement); // Add renderer to HTML as a canvas element
-// // Make Canvas Responsive
-// window.addEventListener('resize', () => {
-//     renderer.setSize(window.innerWidth, window.innerHeight); // Update size
-//     camera.aspect = window.innerWidth / window.innerHeight; // Update aspect ratio
-//     camera.updateProjectionMatrix(); // Apply changes
-// })
-
-
-// // Add ambient light
-// const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-// scene.add(ambientLight);
-
-
-// // Rendering
-// const rendering = function() {
-//     requestAnimationFrame(rendering);
-//     renderer.setClearColor("#000000"); // Black background
-//     renderer.render(scene, camera);
-// }
-// rendering();
