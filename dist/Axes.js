37d00553c259a8fa3878dcff81b9693421823245
Nick Walker <nswalker@cs.washington.edu>
2023-10-06 23:23:22 -0700

    Add twist trace in viz
    Correctly position target marker based on twist application (instead of tranlate + rotate)

--- dist/Axes.js
+++ dist/Axes.js
@@ -5,7 +5,8 @@ import {mergeGeometries} from 'three/addons/utils/BufferGeometryUtils.js'
 
 class Axes extends Mesh {
 
-	constructor( {size: size = 1 , thickness: thickness= .05}) {
+	constructor( {size: size = 1 , thickness: thickness= .05, opacity: opacity = 1}) {
+
 		const xGeometry = new THREE.CylinderGeometry(thickness, thickness, size, 32 );
 		xGeometry.rotateZ(Math.PI / 2)
 		xGeometry.translate(size / 2, 0, 0)
@@ -18,12 +19,13 @@ class Axes extends Mesh {
 		const merged = mergeGeometries([xGeometry, yGeometry, zGeometry], true)
 
 		let material = [
-			new THREE.MeshPhysicalMaterial( {color: 0xff0000} ),
-			new THREE.MeshPhysicalMaterial( {color: 0x00ff00} ),
-			new THREE.MeshPhysicalMaterial( {color: 0x0000ff} )
+			new THREE.MeshPhysicalMaterial( {color: 0xff0000, opacity: opacity, transparent: opacity < 1.0} ),
+			new THREE.MeshPhysicalMaterial( {color: 0x00ff00, opacity: opacity, transparent: opacity < 1.0} ),
+			new THREE.MeshPhysicalMaterial( {color: 0x0000ff, opacity: opacity, transparent: opacity < 1.0} )
 			]
 
 		super( merged, material );
+		this._opacity = opacity
 		this.geometry.groups[0].materialIndex = 0
 		this.geometry.groups[1].materialIndex = 1
 		this.geometry.groups[2].materialIndex = 2
@@ -34,6 +36,9 @@ class Axes extends Mesh {
 		this.material[0].color = new Color(xAxisColor)
 		this.material[1].color = new Color(yAxisColor)
 		this.material[2].color = new Color(zAxisColor)
+		this.material[0].opacity = this._opacity
+		this.material[1].opacity = this._opacity
+		this.material[2].opacity = this._opacity
 		return this;
 	}
 }
--- dist/TwistViz.js
+++ dist/TwistViz.js
@@ -1,6 +1,20 @@
 import * as THREE from 'three'
-import {AxesHelper} from 'three'
 import {Axes} from "./Axes.js";
+
+function integrateTwistStepwise(twist, time, steps) {
+    const [linear, angular] = [new THREE.Vector3(...twist[0]), new THREE.Vector3(...twist[1])]
+    const dt = time / steps
+    let points = [new THREE.Vector3(0,0,0)]
+    let stepRotation = angular.clone().multiplyScalar(dt)
+    let linearStep = linear.clone().multiplyScalar(dt)
+    for (let i = 1; i < steps; i++) {
+        // New point is the previous rotated then translated
+        const newPoint = points[i - 1].clone()
+        newPoint.applyEuler(new THREE.Euler(...stepRotation)).add(linearStep)
+        points.push(newPoint)
+    }
+    return points
+}
 
 export class TwistViz {
 
@@ -8,26 +22,31 @@ export class TwistViz {
         this.container = container
         this.exageration = 1.
         this.scene = new THREE.Scene();
-        this.anchorMarker = new AxesHelper(.5);
-        this.anchorMarker.setColors(0xaa0000, 0x00aa00, 0x000aa);
+        this.anchorMarker = new Axes({size: .5, thickness: 0.025, opacity: 0.25});
+        this.anchorMarker.setColors(0xff0000, 0x00ff00, 0x000ff);
         this.movingMarker = new Axes({size: 1});
         this.movingMarker.setColors(0xff0000, 0x00ff00, 0x000ff);
         this.scene.add(this.movingMarker);
         this.scene.add(this.anchorMarker);
+
+        this.twistTrace = new THREE.Line( 
+            new THREE.BufferGeometry(), 
+            new THREE.LineBasicMaterial( { color: 0x999999, linewidth: 5 } ) );
+
+        this.scene.add(this.twistTrace)
         this._animationRequest = null;
-        // Camera
+
         this.camera = new THREE.PerspectiveCamera(50, container.innerWidth / container.innerHeight, 0.6, 1000);
         this.camera.up.set(0,0,1)
-        // Renderer
         this.renderer = new THREE.WebGLRenderer({antialias: true});
         this.renderer.setClearColor("#FFFFFF"); // Set background colour
         container.appendChild(this.renderer.domElement); // Add renderer to HTML as a
         this.renderer.setSize(container.clientWidth, container.clientHeight);
-        // Add ambient light
         const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
         const directionalLight = new THREE.DirectionalLight( 0xffffff, 1.5);
-        directionalLight.position.x = -10
-        directionalLight.position.z = 10
+        directionalLight.position.x = -2
+        directionalLight.position.y = -2
+        directionalLight.position.z = 2
 
         this.scene.add(ambientLight);
         this.scene.add(directionalLight)
@@ -47,10 +66,12 @@ export class TwistViz {
     }
 
     setTwist([linear, angular]) {
+        const twistTracePoints = integrateTwistStepwise([linear, angular], 1, 100)
+        this.twistTrace.geometry.setFromPoints(twistTracePoints)
+
         let marker = this.movingMarker
-        marker.position.x = linear[0] * this.exageration
-        marker.position.y = linear[1] * this.exageration
-        marker.position.z = linear[2] * this.exageration
+        const finalPoint = twistTracePoints[twistTracePoints.length - 1]
+        marker.position.copy(finalPoint)
 
         marker.rotation.x = angular[0] 
         marker.rotation.y = angular[1]
